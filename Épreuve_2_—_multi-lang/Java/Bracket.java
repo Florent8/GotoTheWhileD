fcfcb34f718a8666a3543f3ded66d79259d3a2b3
Florent8 <florent.martin02@edu.univ-fcomte.fr>
2017-12-21 19:20:00 +0100

    Une pile est plus appropriée qu'une liste

--- Épreuve_2_—_multi-lang/Java/Bracket.java
+++ Épreuve_2_—_multi-lang/Java/Bracket.java
@@ -1,31 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 import static java.lang.System.exit;
 import static java.lang.System.out;
 
 public final class Bracket {
 
-    private static final List<Character> characters = new ArrayList<>();
+    private static final Stack<Character> characters = new Stack<>();
 
     public static void main(final String[] args) {
         final char[] chars = args[0].toCharArray();
         for (final char c : chars)
             switch (c) {
                 case '(':
-                    characters.add('(');
+                    characters.push('(');
                     break;
                 case ')':
                     checkBracket('(');
                     break;
                 case '{':
-                    characters.add('{');
+                    characters.push('{');
                     break;
                 case '}':
                     checkBracket('{');
                     break;
                 case '[':
-                    characters.add('[');
+                    characters.push('[');
                     break;
                 case ']':
                     checkBracket('[');
@@ -38,10 +37,9 @@ public final class Bracket {
     }
 
     private static void checkBracket(final char bracket) {
-        if (characters.get(characters.size() - 1) != bracket) {
+        if (characters.pop() != bracket) {
             out.println("NO");
             exit(0);
         }
-        characters.remove(characters.size() - 1);
     }
 }
