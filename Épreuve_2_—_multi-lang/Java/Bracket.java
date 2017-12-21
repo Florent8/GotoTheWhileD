40947dc7f2adfabfdb9aaa265092af6f74edd0e2
Florent8 <florent.martin02@edu.univ-fcomte.fr>
2017-12-21 20:05:51 +0100

    Pile/File + condensation du code

--- Épreuve_2_—_multi-lang/Java/Bracket.java
+++ Épreuve_2_—_multi-lang/Java/Bracket.java
@@ -1,45 +1,32 @@
-import java.util.Stack;
+import java.util.*;
 
-import static java.lang.System.exit;
 import static java.lang.System.out;
 
 public final class Bracket {
 
-    private static final Stack<Character> characters = new Stack<>();
+    private static final List<Character>  OPEN_BRACKETS  = Arrays.asList('(', '{', '[');
+    private static final Stack<Character> openBrackets   = new Stack<>();
+    private static final List<Character>  CLOSE_BRACKETS = Arrays.asList(')', '}', ']');
+    private static final Queue<Character> closeBrackets  = new ArrayDeque<>();
 
     public static void main(final String[] args) {
-        final char[] chars = args[0].toCharArray();
-        for (final char c : chars)
-            switch (c) {
-                case '(':
-                    characters.push('(');
-                    break;
-                case ')':
-                    checkBracket('(');
-                    break;
-                case '{':
-                    characters.push('{');
-                    break;
-                case '}':
-                    checkBracket('{');
-                    break;
-                case '[':
-                    characters.push('[');
-                    break;
-                case ']':
-                    checkBracket('[');
-                    break;
-            }
-        if (characters.isEmpty())
-            out.println("YES");
-        else
-            out.println("NO");
+        out.println(checkBracket(args[0].toCharArray()) ? "YES" : "NO");
     }
 
-    private static void checkBracket(final char bracket) {
-        if (characters.pop() != bracket) {
-            out.println("NO");
-            exit(0);
-        }
+    private static boolean checkBracket(final char[] chars) {
+        for (final char c : chars)
+            if (OPEN_BRACKETS.contains(c))
+                openBrackets.push(c);
+            else if (CLOSE_BRACKETS.contains(c))
+                closeBrackets.offer(c);
+
+        if (openBrackets.size() != closeBrackets.size())
+            return false;
+
+        while (!openBrackets.isEmpty())
+            if (closeBrackets.poll() != CLOSE_BRACKETS.get(OPEN_BRACKETS.indexOf(openBrackets.pop())))
+                return false;
+
+        return true;
     }
 }
