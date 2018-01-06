00e6e18b1679ec106dd03a4bcb69bcebce291f39
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-01-06 17:56:34 +0100

    Triangle isocèle

--- Épreuve_1_—_One-lining/Sierpinski.java
+++ Épreuve_1_—_One-lining/Sierpinski.java
@@ -13,6 +13,8 @@ public class Sierpinski {
                     triangle[row][col] = triangle[row - 1][col - 1] + triangle[row - 1][col];
         }
         for (int row = 0; row < size; row++) {
+            for (int col = 0; col < (size - row) / 2; col++)
+                System.out.print(" ");
             for (int col = 0; col <= row; col++)
                 System.out.print(triangle[row][col] % 2 == 0 ? " " : "*");
             System.out.println();
