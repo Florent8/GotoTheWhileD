ca5e978660c0212d6cdfa12098663a65b2fb48a5
Florent8 <florent.martin02@edu.univ-fcomte.fr>
2018-01-06 18:15:22 +0100

    Optimisation de l'affichage

--- Épreuve_1_—_One-lining/Sierpinski.java
+++ Épreuve_1_—_One-lining/Sierpinski.java
@@ -4,20 +4,19 @@ public class Sierpinski {
         int size = (int) Math.pow(2, Double.parseDouble(args[0])), triangle[][] = new int[size][];
         triangle[0] = new int[1];
         triangle[0][0] = 1;
-        for (int row = 1; row < size; row++) {
-            triangle[row] = new int[triangle[row - 1].length + 1];
-            for (int col = 0; col <= row; col++)
-                if ((col == 0) || (col == row))
-                    triangle[row][col] = 1;
-                else
-                    triangle[row][col] = triangle[row - 1][col - 1] + triangle[row - 1][col];
-        }
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < (size - row) / 2; col++)
                 System.out.print(" ");
-            for (int col = 0; col <= row; col++)
-                System.out.print(triangle[row][col] % 2 == 0 ? " " : "*");
-            System.out.println();
+            if (row == 0)
+                System.out.println("*");
+            else {
+                triangle[row] = new int[triangle[row - 1].length + 1];
+                for (int col = 0; col <= row; col++) {
+                    triangle[row][col] = (col == 0) || (col == row) ? 1 : triangle[row - 1][col - 1] + triangle[row - 1][col] % 2;
+                    System.out.print(triangle[row][col] % 2 == 0 ? " " : "*");
+                }
+                System.out.println();
+            }
         }
     }
 }
