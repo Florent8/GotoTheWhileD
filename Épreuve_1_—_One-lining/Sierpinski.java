a5329253742bf3083324199ecdf2da629373f991
Florent8 <florent.martin02@edu.univ-fcomte.fr>
2018-01-06 17:02:04 +0100

    Nouvel algorithme pour Sierpinski -> tous les triangles s'affichent

--- Épreuve_1_—_One-lining/Sierpinski.java
+++ Épreuve_1_—_One-lining/Sierpinski.java
@@ -1,36 +1,20 @@
 public class Sierpinski {
 
     public static void main(String[] args) {
-        int max = 1;
-        int lig = Integer.parseInt(args[0]);
-
-        while (max < lig) {
-            max *= 2;
+        int size = (int) Math.pow(2, Double.parseDouble(args[0])), triangle[][] = new int[size][];
+        triangle[0] = new int[1];
+        triangle[0][0] = 1;
+        for (int row = 1; row < size; row++) {
+            triangle[row] = new int[triangle[row - 1].length + 1];
+            for (int col = 0; col <= row; col++)
+                if ((col == 0) || (col == row))
+                    triangle[row][col] = 1;
+                else
+                    triangle[row][col] = triangle[row - 1][col - 1] + triangle[row - 1][col];
         }
-
-        for (int i = lig - 1; i >= 0; i--) {
-            for (int k = 0; k < i / 2; k++) {
-                System.out.print(" ");
-            }
-
-            for (int j = 0; j < max - i; j++) {
-                int ii = i;
-                int jj = j;
-                int div = max;
-
-                while (div > 1 && ii + jj < div) {
-                    div /= 2;
-                    ii = ii % div;
-                    jj = jj % div;
-                }
-
-                if (div == 1) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col <= row; col++)
+                System.out.print(triangle[row][col] % 2 == 0 ? " " : "*");
             System.out.println();
         }
     }
